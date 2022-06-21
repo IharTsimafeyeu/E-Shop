247b06ababde6e996a618ee36b9ad9e8897e4dba
Ihar Tsimafeyeu <ColdSun1395@gmail.com>
2022-06-21 21:40:22 +0400

    initial commit

---
+++ EShop/EShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  EShopApp.swift
+//  EShop
+//
+//  Created by Игорь Тимофеев on 21.06.22.
+//
+
+import SwiftUI
+
+@main
+struct EShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
