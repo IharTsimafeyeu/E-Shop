0af3965ce106bb7bfb8c0d6bcd30a7181a2efd42
Ihar Tsimafeyeu <ColdSun1395@gmail.com>
2022-06-26 22:45:57 +0400

    Initial commit

---
+++ EShop/Views/InitialView.swift
@@ -0,0 +1,16 @@
+import SwiftUI
+
+struct InitialView: View {
+    var body: some View {
+        VStack {
+        
+        }
+        
+    }
+}
+
+struct InitialView_Previews: PreviewProvider {
+    static var previews: some View {
+        InitialView()
+    }
+}
